8307492e89ca34afa86cdc0df9518d5ae9f84823
Jose Garboza <jgarboza813@gmail.com>
2021-02-10 06:50:57 -0500

    Flex -> Grid

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -6,32 +6,20 @@ const MainPage = () => {
     return (
         <div className={styles.root}>
             <div className={styles.parent}>
+                <div className={styles.name}>
+                    Jose Garboza
+                </div>
+                <div className={styles.detailTop}/>
                 <div className={styles.stats}>
-                    <div className={styles.name}>
-                        Jose Garboza
-                    </div>
-                    <br />
-                    <div>
-                        Web Developer
-                    </div>
-                    <div>
-                        Washington, DC
-                    </div>
-                    <br />
-                    <div>
-                        React
-                    </div>
-                    <div>
-                        Vanilla JavaScript
-                    </div>
-                    <div>
-                        HTML/CSS/SCSS
-                    </div>
+                    <p>Web Developer<br/>Washington, DC</p>
+                    <p><br/>React<br/>ES6<br/>HTML/CSS/SASS</p>
                 </div>
                 <div className={styles.details}>
-                    <br />
-                    <br />
-                    Hello, my name is Jose. I am a visual learner and a developer.
+                    <p>Hello, my name is Jose. I have 17 years of experience developing applications.</p>
+                    <p>I am a strong believer in technology solving our problems instead of adding to them. I am specially interested in making difficult problems easier to solve through intuitive UIs.</p>
+                    <p>As the web keeps expanding at break-neck speed, building web applications is becoming more complex.
+                        The number of abstractions that web developers need to understand is growing day by day.</p>
+                    <p><br/>The future of the web will be dictated by how powerful our development tools and frameworks are.</p>
                 </div>
             </div>
         </div>
